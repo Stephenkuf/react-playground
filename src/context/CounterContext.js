ad339fb7ff2c5d1b75f345fcb6e320428c9d563b
Stephen Kuforiji <stephensleem@gmail.com>
2022-10-18 09:22:02 +0100

    API calls

--- src/context/CounterContext.js
+++ src/context/CounterContext.js
@@ -41,7 +41,6 @@ export const CounterContextProvider = ({ children }) => {
       localStorage.setItem('counterAppTheme', "light");
     }
   }
-
   function globalIncrement() {
     setGlobalCount((globalCount) => globalCount + 1)
   }
